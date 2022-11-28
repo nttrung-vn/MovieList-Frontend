58cff88c8656aae1ce340aaab8ae11877c4ac709
nttrung <nttrung.me@gmail.com>
2022-11-29 05:26:03 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: "movielist",
     component: MovieList,
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page" style="text-align: center">
+    <p class="my-3">
+      Oops, the page could not be found. <br />
+      Return to
+      <router-link to="/">homepage.</router-link>
+    </p>
+  </div>
+</template>
